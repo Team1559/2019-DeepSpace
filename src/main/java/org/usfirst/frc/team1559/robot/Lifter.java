73374898c32a04ed5fbb0c530fa262be19376a41
Evan Gartley <egartley@users.noreply.github.com>
2019-01-09 11:21:16 -0500

    Add files via upload

--- src/main/java/org/usfirst/frc/team1559/robot/Lifter.java
+++ src/main/java/org/usfirst/frc/team1559/robot/Lifter.java
@@ -2,26 +2,71 @@ package org.usfirst.frc.team1559.robot;
 
 public class Lifter {
 	
+	public static final int POS1 = 1;
+	public static final int POS2 = 2;
+	public static final int POS3 = 3;
+	public static final int POS4 = 4;
+	public static final int POS_HOME = 99;
+
+	public boolean isEmpty = true;
+
 	public Lifter() {
 		// motors, ids, etc
 	}
 
+	public void debug() {
+		// output things for debugging such as motor/encoder values
+	}
+
 	/**
-	 * Move upwards, from the current position
+	 * Go to the specified rocket opening (1, 2 or 3)
+	 *      __
+	 *    /    \
+	 *    | [] |    3
+	 *    |    |
+	 *    | [] |    2
+	 *   /|    |\
+	 *  / | [] | \  1
+	 */
+	public void rocket(int n) {
+		switch (n) {
+			case 1:
+				// window 1
+				break;
+			case 2:
+				// window 2
+				break;
+			case 3:
+				// window 3
+				break;
+			default:
+				// unknown
+		}
+	}
+
+	/**
+	 * Go to the height of the cargo bay
+	 */
+	public void cargoBay() {
+		
+	}
+
+	/**
+	 * Tele-operated upward movement, from the current position
 	 */
 	public void up() {
 
 	}
 
 	/**
-	 * Move upwards, the specified amount, from the current position
+	 * Move downwards, the specified amount, from the current position
 	 */
 	public void up(int n) {
 
 	}
 
 	/**
-	 * Move downwards, from the current position
+	 * Tele-operated downward movement, from the current position
 	 */
 	public void down() {
 
@@ -38,11 +83,29 @@ public class Lifter {
 	 * Moves to the "home" position
 	 */
 	public void home() {
-
+		moveTo(POS_HOME);
 	}
 
 	public void moveTo(int positionNumber) {
-
+		switch (positionNumber) {
+			case POS1:
+				// pos 1
+				break;
+			case POS2:
+				// pos 2
+				break;
+			case POS3:
+				// pos 3
+				break;
+			case POS4:
+				// pos 4
+				break;
+			case POS_HOME:
+				// pos home
+				break;
+			default:
+				// unknown pos
+		}
 	}
 
 }
