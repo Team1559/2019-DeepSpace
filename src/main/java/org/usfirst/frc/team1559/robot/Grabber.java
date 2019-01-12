d4008c8276e334bdb4cabff1b10204512f34177c
Jason-VL <44625025+Jason-VL@users.noreply.github.com>
2019-01-12 13:41:20 -0500

    Update Grabber.java

--- src/main/java/org/usfirst/frc/team1559/robot/Grabber.java
+++ src/main/java/org/usfirst/frc/team1559/robot/Grabber.java
@@ -5,7 +5,16 @@ public class Grabber {
 	 * Designated Programmers of the Grabber:
 	 * Jason Vander Linden
 	 * XXX
-	 * 
+	 * /**
+	 * Go to the specified rocket port (1, 2 or 3)
+	 *      __
+	 *    /    \
+	 *    | [] |    3 (6 ft, 11.5 in)
+	 *    |    |
+	 *    | [] |    2 (4 ft, 7.5 in)
+	 *   /|    |\
+	 *  / | [] | \  1 (2 ft, 3.5 in)
+	 
 	 * The Grabber class is designed to implement the functionality of grabbing cargo and the orange hatch as one system.
 	 * 
 	 */
