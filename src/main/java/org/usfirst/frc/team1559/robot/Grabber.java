502280f09260e82a660094611793225761aa63c4
Jason-VL <44625025+Jason-VL@users.noreply.github.com>
2019-01-12 11:43:06 -0500

    VS Code new member test
    Making sure VS code is working on my laptop

--- src/main/java/org/usfirst/frc/team1559/robot/Grabber.java
+++ src/main/java/org/usfirst/frc/team1559/robot/Grabber.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team1559.robot;
 public class Grabber {
 	/*
 	 * Designated Programmers of the Grabber:
-	 * XXX
+	 * Jason Vander Linden
 	 * XXX
 	 * 
 	 * The Grabber class is designed to implement the functionality of grabbing cargo and the orange hatch as one system.
