88deb3a7f8fdb37e3bf6bd64c1f635c530e52e39
Lucariax <lucariax@hotmail.com>
2019-01-12 16:04:39 -0500

    Working Tele
    *Updated Talon Ports

--- src/main/java/org/usfirst/frc/team1559/robot/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team1559/robot/DriveTrain.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team1559.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
-public class DriveTrain {
+public class DriveTrain 
+{
 	/*
 	 * Designated Programmers of the DriveTrain:
 	 * Michael Nersinger
@@ -17,16 +19,28 @@ public class DriveTrain {
 	private MecanumDrive drive;
 	private WPI_TalonSRX frontLeft, rearLeft, frontRight, rearRight;
 
-	public DriveTrain() {
+	public DriveTrain() 
+	{
 		frontLeft = new WPI_TalonSRX(Wiring.FRONT_LEFT_MOTOR);
 		rearLeft = new WPI_TalonSRX(Wiring.REAR_LEFT_MOTOR);
 		frontRight = new WPI_TalonSRX(Wiring.FRONT_RIGHT_MOTOR);
 		rearRight = new WPI_TalonSRX(Wiring.REAR_RIGHT_MOTOR);
 
+		frontLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+		rearLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+		frontRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+		rearRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+
+		frontLeft.set(ControlMode.Velocity, 0);
+		rearLeft.set(ControlMode.Velocity, 0);
+		frontRight.set(ControlMode.Velocity, 0);
+		rearRight.set(ControlMode.Velocity, 0);
+
 		drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 	}
 
-	public void driveCartesian(double ySpeed, double xSpeed, double zRotation) {
+	public void driveCartesian(double ySpeed, double xSpeed, double zRotation) 
+	{
 		drive.driveCartesian(ySpeed, xSpeed, zRotation);
 	}
 }
--- src/main/java/org/usfirst/frc/team1559/robot/Wiring.java
+++ src/main/java/org/usfirst/frc/team1559/robot/Wiring.java
@@ -19,10 +19,10 @@ public interface Wiring {
 	 */
 	
 	// DriveTrain
-	public static final int FRONT_LEFT_MOTOR = 10;
-	public static final int FRONT_RIGHT_MOTOR = 11;
-	public static final int REAR_LEFT_MOTOR = 12;
-	public static final int REAR_RIGHT_MOTOR = 13;
+	public static final int FRONT_LEFT_MOTOR = 0;
+	public static final int FRONT_RIGHT_MOTOR = 1;
+	public static final int REAR_LEFT_MOTOR = 2;
+	public static final int REAR_RIGHT_MOTOR = 3;
 	
 	// Grabber
 	
