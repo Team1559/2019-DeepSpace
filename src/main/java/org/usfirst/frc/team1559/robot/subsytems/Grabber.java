b9555d4221c40fa9926a4f552e5c2c5ecc9e9b7c
hnh <hannahwilcox6@gmail.com>
2019-01-12 14:20:10 -0500

    lil edits

--- src/main/java/org/usfirst/frc/team1559/robot/subsytems/Grabber.java
+++ src/main/java/org/usfirst/frc/team1559/robot/subsytems/Grabber.java
@@ -3,4 +3,5 @@ package org.usfirst.frc.team1559.robot;
 import src.org.usfirst.frc.team1559.robot.Robot;
 import src.org.usfirst.frc.team1559.robot.Wiring;
 
+import edu.wpi.first.wpilibj.solenoid
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; 
