6e6f712915adbec0c6f1393a3226f8259b794a5a
SonicDRJ <32721112+Jakkuman@users.noreply.github.com>
2019-01-24 20:08:26 -0500

    Fixing button problem
    Fixed the issue where the code asked for more buttons than we actually had available to us.

--- src/main/java/org/usfirst/frc/team1559/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1559/robot/Robot.java
@@ -36,6 +36,7 @@ public class Robot extends TimedRobot {
 		DistSensor dSensor = new DistSensor();
 		dSensor.setAutomaticMode(true);
 		dSensor.stopRobot();
+		lifter = new Lifter();
 	}
 
 	@Override
@@ -55,33 +56,65 @@ public class Robot extends TimedRobot {
 
 
 		//LIFTER
-		lifter = new Lifter();
+		
+		// if(oi.getCopilotButton(0).isPressed()) {
+		// 	lifter.goToPortPos(1);
+		// }
+		// else if(oi.getCopilotButton(1).isPressed()) {
+		// 	lifter.goToPortPos(2);
+		// }
+		// else if(oi.getCopilotButton(2).isPressed()) {
+		// 	lifter.goToPortPos(3);
+		// }
+		// else if(oi.getCopilotButton(3).isPressed()) {
+		// 	lifter.goToHatchPos(1);
+		// }
+		// else if(oi.getCopilotButton(4).isPressed()) {
+		// 	lifter.goToHatchPos(2);
+		// }
+		// else if(oi.getCopilotButton(5).isPressed()) {
+		// 	lifter.goToHatchPos(3);
+		// }
+		// else if(oi.getCopilotButton(6).isPressed()) {
+		// 	lifter.goToCargoShipCargoDrop();
+		// }
+		// else if(oi.getCopilotButton(7).isPressed()) {
+		// 	lifter.goToCargoShipHatch();
+		// }
+		// else if(oi.getCopilotButton(8).isPressed()) {
+		// 	lifter.goToBottom();
+		// }
+		int pos = 0;
+		if(oi.getCopilotButton(2).isPressed()) {
+			pos++;
+		}
 		if(oi.getCopilotButton(0).isPressed()) {
+			pos--;
+		}
+		switch(pos) {
+			case 0:
+			lifter.goToBottom();
+			break;
+			case 1:
+			lifter.goToCargoShipHatch(); //Same as hatch position 1
+			break;
+			case 2:
 			lifter.goToPortPos(1);
-		}
-		else if(oi.getCopilotButton(1).isPressed()) {
+			break;
+			case 3:
+			lifter.goToCargoShipCargoDrop(); //Same as hatch position 2
+			break;
+			case 4:
 			lifter.goToPortPos(2);
-		}
-		else if(oi.getCopilotButton(2).isPressed()) {
+			break;
+			case 5:
+			lifter.goToHatchPos(3);
+			break;
+			case 6:
 			lifter.goToPortPos(3);
 		}
-		else if(oi.getCopilotButton(3).isPressed()) {
-			lifter.goToHatchPos(1);
-		}
-		else if(oi.getCopilotButton(4).isPressed()) {
-			lifter.goToHatchPos(2);
-		}
-		else if(oi.getCopilotButton(5).isPressed()) {
-			lifter.goToHatchPos(3);
-		}
-		else if(oi.getCopilotButton(6).isPressed()) {
-			lifter.goToCargoShipCargoDrop();
-		}
-		else if(oi.getCopilotButton(7).isPressed()) {
-			lifter.goToCargoShipHatch();
-		}
-		else if(oi.getCopilotButton(8).isPressed()) {
-			lifter.goToBottom();
+		if(oi.getCopilotButton(7).isPressed()) {
+			lifter.recallibrateSystem();
 		}
 
 	}
