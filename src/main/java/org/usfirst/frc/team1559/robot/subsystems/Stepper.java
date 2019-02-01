cec085a99735ef1c73ada0889d8946a8cce6a93c
nickmerante <44625188+nickmerante@users.noreply.github.com>
2019-02-01 17:49:51 -0500

    Stepper Skeleton Code
    Basic Code for the Stepper Class

--- src/main/java/org/usfirst/frc/team1559/robot/subsystems/Stepper.java
+++ src/main/java/org/usfirst/frc/team1559/robot/subsystems/Stepper.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team1559.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import org.usfirst.frc.team1559.robot.Wiring;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class Stepper {
 	/*
 	 * Designated Programmers of the Stepper:
@@ -37,4 +43,46 @@ public class Stepper {
 	 * "One small step for man. One giant leap for mankind"
 	 * 		-Neil Armstrong
 	 */
+
+	//Fr this code, the back of the robot will be called the front and vice versa, as the robot
+	//will be backing onto the step. All names are based on this fact.
+
+	private TalonSRX lifterMotor;
+	private TalonSRX rotationalMotor;
+	private TalonSRX driveMotor;
+	private Solenoid leftPiston;
+	private Solenoid rightPiston;
+
+	public Stepper()
+	{
+		lifterMotor = new TalonSRX(Wiring.STEPPER_LIFTER_MOTOR);
+		rotationalMotor = new TalonSRX(Wiring.STEPPER_ROTATIONAL_MOTOR);
+		driveMotor = new TalonSRX(Wiring.STEPPER_DRIVE_MOTOR);
+		leftPiston = new Solenoid(Wiring.STEPPER_LEFT_PISTON);
+		rightPiston = new Solenoid(Wiring.STEPPER_RIGHT_PISTON);
+	}
+
+	public void liftBack()
+	{
+		leftPiston.set(true);
+		rightPiston.set(true);
+	}
+	
+	//moves lifterMotor to correct position for rotationalMotor
+	public void positionLifter()
+	{
+		
+	}
+
+	//have back rotationalMotor spin to lift front
+	public void liftFront()
+	{
+		
+	}
+
+	//drives the front wheels
+	public void drive()
+	{
+		
+	}
 }
