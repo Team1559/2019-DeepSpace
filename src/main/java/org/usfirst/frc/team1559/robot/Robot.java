c14ba4a80189c90be824facfe1ee286df16474f9
Lucariax <lucariax@hotmail.com>
2019-02-02 08:32:43 -0500

    Removed Fine Control
    - Fine Control

--- src/main/java/org/usfirst/frc/team1559/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1559/robot/Robot.java
@@ -77,7 +77,6 @@ public class Robot extends TimedRobot {
 		// Camera
 		
 		// Drive Train
-		oi.checkFineControl();
 		drive.driveCartesian(oi.getPilotY(), oi.getPilotX(), oi.getPilotZ());
 
 		// Grabber
