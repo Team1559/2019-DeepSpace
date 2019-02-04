ed84b38190a1cd5be507b051c3386fc97fbf718b
alekahrens <scaaja@gmail.com>
2019-02-04 16:33:26 -0500

    Update VisionData.java

--- src/main/java/org/usfirst/frc/team1559/robot/VisionData.java
+++ src/main/java/org/usfirst/frc/team1559/robot/VisionData.java
@@ -1,9 +1,17 @@
 package org.usfirst.frc.team1559.robot;
 
 public class VisionData {
-
+    
     public double x;
     public double y;
     public double r;
     public Integer status;
+
+    public boolean IsValid(){
+        if(status!=1){
+            return false;
+        }
+        else
+            return true;
+    }
 }
