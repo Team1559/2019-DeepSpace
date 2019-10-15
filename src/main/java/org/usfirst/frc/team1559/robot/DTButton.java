b9af44c757ef9fe402836491375ac27311eef006
Jason-VL <44625025+Jason-VL@users.noreply.github.com>
2019-10-15 19:51:31 -0400

    Update DTButton.java

--- src/main/java/org/usfirst/frc/team1559/robot/DTButton.java
+++ src/main/java/org/usfirst/frc/team1559/robot/DTButton.java
@@ -11,7 +11,7 @@ public class DTButton {
 
     public DTButton(Joystick stick, int button) {
         this.stick = stick;
-        this.button = button;// this is a test
+        this.button = button;
     }
 
     public void update() {
